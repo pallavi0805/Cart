6a2b4e85e72e3b8e5fe0addedc59778d3c165214
ibm <ibm@avijroy8>
2016-09-23 14:05:51 +0530

    a

---
+++ JSF2/src/com/model/Address.java
@@ -0,0 +1,86 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Address 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="mysequence")
+	@SequenceGenerator(name="mysequence", sequenceName="ADDR_SEQ", allocationSize=1)
+	@Column(name="ADDRESS_ID")
+	int addressId;
+	
+	String name;
+	
+	String address1;
+	
+	String address2;
+	
+	String city;
+	
+	String state;
+	
+	int pin;
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}	
+}
